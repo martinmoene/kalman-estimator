5e14c97c006df4559b035a7d6fcc6c2b3b5521c9
Martin Moene <martin.moene@gmail.com>
2018-06-26 10:08:16 +0200

    Rename to kalman-sim

--- example/mk-kalman-class.bat
+++
@@ -1,2 +0,0 @@
-cl -EHsc -I../include kalman-class.cpp && kalman-class.exe
-kalman-class.exe > kalman-class.out.txt
---
+++ example/mk-kalman-sim.bat
@@ -0,0 +1,2 @@
+cl -EHsc -I../include kalman-sim.cpp && kalman-sim.exe
+kalman-sim.exe > kalman-sim.out.txt
