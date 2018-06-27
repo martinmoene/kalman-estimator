90196e0b5f3bcbd3cfe709795a3ab399831b6e1e
Martin Moene <martin.moene@gmail.com>
2018-06-27 23:17:15 +0200

    Put echo off

--- test/t-avr.bat
+++ test/t-avr.bat
@@ -1,4 +1,10 @@
+@echo off & setlocal enableextensions enabledelayedexpansion
+::
+:: t-avr.bat - compile & run tests (AVR-GCC).
+::
+
 :: atmega328: avr5 - "Enhanced" devices with 16 KiB up to 64 KiB of program memory.
+
 set opt=
 if [%1] == [] set opt=-O2
 
@@ -6,3 +12,5 @@ set KE_NUMERIC_TYPE=double
 ::set KE_NUMERIC_TYPE=fp32_t
 
 call %~dp0..\script\avr.bat -std=c++17 -Wall -I../include %opt% %* -mmcu=avr5 -S -DAVR=1 -DKE_NUMERIC_TYPE=%KE_NUMERIC_TYPE% main-avr.t.cpp
+
+endlocal & goto :EOF
