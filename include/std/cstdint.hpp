34aa45662b68cf72a4c4b1b5fa43c6630b4ca5bd
Martin Moene <martin.moene@gmail.com>
2018-06-27 17:12:39 +0200

    Use type macros

--- include/std/cstdint.hpp
+++ include/std/cstdint.hpp
@@ -10,10 +10,10 @@
 
 namespace std {
 
-using int8_t  = signed char;
-using int16_t = signed short;
-using int32_t = signed long;
-using int64_t = signed long long;
+using int8_t  = __INT8_TYPE__;
+using int16_t = __INT16_TYPE__;
+using int32_t = __INT32_TYPE__;
+using int64_t = __INT64_TYPE__;
 
 }
 
