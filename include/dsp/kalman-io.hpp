dfa0c0439ac1407856c405257ff911671fcf4f8e
Martin Moene <martin.moene@gmail.com>
2018-09-24 10:48:37 +0200

    Update include folder

--- include/dsp/kalman-io.hpp
+++ include/dsp/kalman-io.hpp
@@ -3,12 +3,12 @@
 //
 // https://github.com/martinmoene/kalman-estimator
 //
-// Distributed under the Boost Software License, Version 1.0. 
+// Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef NUM_KALMAN_IO_HPP_INCLUDED
 #define NUM_KALMAN_IO_HPP_INCLUDED
 
-#include "matrix-io.hpp"
+#include "num/matrix-io.hpp"
 
 #endif // NUM_KALMAN_IO_HPP_INCLUDED
