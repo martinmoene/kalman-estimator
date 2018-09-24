f19eac7534d9f7c1d8b192f89122b0256765677e
Martin Moene <martin.moene@gmail.com>
2018-09-25 00:35:23 +0200

    Update biquad.cpp for core::range()

--- example/desktop/biquad.cpp
+++ example/desktop/biquad.cpp
@@ -6,42 +6,30 @@
 // (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include "dsp/biquad-io.hpp"
+#include "core/range.hpp"
+#include "core/text.hpp"
 #include <algorithm>
 #include <string>
 #include <vector>
 
 const auto Nsamp = 100;     // print this number of samples
 
-using Count = int;
-using Text  = std::string;
+using core::Text;
+using core::line;
 
+using Count = int;
 using value_type = double;
 
 using BiQuad = dsp::BiQuadT<value_type>;
-
-// generate range [first, last] in cnt steps:
-
-template< typename T, typename U >
-auto range( T first, T last , U cnt )
-{
-    const auto step = (last - first) / (cnt - 1);
-
-    std::vector<value_type> result( cnt );
-
-    std::generate(
-        result.begin(), result.end(),
-        [step, n = 0] () mutable { return step * n++; }
-    );
-
-    return result;
-}
 
 template< typename Rng >
 auto print( Rng rng, Text text, Text design, BiQuad && bq )
 {
     std::cout <<
+        "\n"   << line(115, '=') <<
         "\n" << text << " " << design << ":\n" << bq <<
-        "\nFilter magnitude response for normalized frequency:\n" <<
+        "\n% " << line(65, '-') <<
+        "\n% Matlab/Octave filter magnitude response for normalized frequency:\n" <<
         "\nfn = [";
 
     for( auto fn : rng )
@@ -60,18 +48,28 @@ auto print( Rng rng, Text text, Text design, BiQuad && bq )
 
     std::cout <<
         "];\n"
-        "\nplot(fn, y);\n"
+        "\nplot(fn, y);"
         "\n" <<
-        "\n% Filter design:\n" <<
+        "\n% " << line(42, '-') <<
+        "\n% Corresponding Matlab/Octave filter design:" <<
+        "\n"   <<
+        "\npkg load signal\n" <<
         design <<
-        "\nbq = tf2sos(b,a)\n"
+        "\nbq = tf2sos(b,a)"
         "\nfreqz(b,a);\n";
 }
 
 int main()
 {
-    print( range( 0.0, 0.5, Nsamp ), "Bi-quad filter design", "[b,a] = cheby1(2,3,0.4)" , BiQuad{ { 0.14361 , 0.28721 , 0.14361  }, { -0.67993, 0.49133 } } );
-    print( range( 0.0, 0.5, Nsamp ), "Bi-quad filter design", "[b,a] = cheby2(2,10,0.4)", BiQuad{ { 0.323496, 0.017539, 0.323496 }, { -0.66299, 0.32752 } } );
+    using core::range;
+
+    const auto Flo   = 0.0;
+    const auto Fhi   = 0.5;
+    const auto Nsamp = 201;
+    const auto Fstep = (Fhi - Flo) / (Nsamp - 1);
+
+    print( range( Flo, Fhi + Fstep, Fstep ), "Bi-quad filter design", "[b,a] = cheby1(2,3,0.4)" , BiQuad{ { 0.14361 , 0.28721 , 0.14361  }, { -0.67993, 0.49133 } } );
+    print( range( Flo, Fhi + Fstep, Fstep ), "Bi-quad filter design", "[b,a] = cheby2(2,10,0.4)", BiQuad{ { 0.323496, 0.017539, 0.323496 }, { -0.66299, 0.32752 } } );
 }
 
 // cl -EHsc -std:c++17 -O2 -I../../include biquad.cpp && biquad.exe
