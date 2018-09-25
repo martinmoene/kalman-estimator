dc339ef6bcfe25e1f06c0855f24823b46b78fbb5
Martin Moene <martin.moene@gmail.com>
2018-09-25 10:38:20 +0200

    Add header for core module

---
+++ include/core/core.hpp
@@ -0,0 +1,18 @@
+// Copyright 2018 by Martin Moene
+//
+// https://github.com/martinmoene/kalman-estimator
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+// This code is based on cpp11-range by Konrad Rudolph,
+// https://github.com/klmr/cpp11-range
+
+#ifndef CORE_CORE_HPP_INCLUDED
+#define CORE_CORE_HPP_INCLUDED
+
+#include "range.hpp"
+#include "text.hpp"
+#include "vector-io.hpp"
+
+#endif // CORE_CORE_HPP_INCLUDED
